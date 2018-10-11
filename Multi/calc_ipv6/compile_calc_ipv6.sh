8ba4c31ee1ca6a02ee7a24867c5f134028db7e99
Scourge <Scourge@protonmail.com>
2018-10-11 09:51:27 -0400

    calc_ipv6

--- Multi/calc_ipv6/compile_calc_ipv6.sh
+++ Multi/calc_ipv6/compile_calc_ipv6.sh
@@ -2,5 +2,5 @@
 
 
 
-x86_64-linux-gnu-g++ -g calc_ipv6.cpp -o calc_ipv6 -std=c++17 -Wfatal-errors -lOS -lre
+x86_64-linux-gnu-g++ -O2 calc_ipv6.cpp -o calc_ipv6 -std=c++17 -Wfatal-errors -lOS -lre
 
