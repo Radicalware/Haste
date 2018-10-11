e9346f69120249fa74861435cf379f24865c19ac
Scourge <Scourge@protonmail.com>
2018-10-10 23:43:37 -0400

    calc_ipv6

---
+++ Multi/calc_ipv6/calc_ipv6.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <bitset>
+
+#include "OS.h"   // Found on "https://github.com/Radicalware"
+#include "ord.h"  // Found on "https://github.com/Radicalware"
+#include "re.h"   // Found on "https://github.com/Radicalware"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main(int argc, char** argv){
+
+	os.set_args(argc, argv);
+
+	if (argc == 1 || os.has_key("-mac") == false || os.has_key("-ipv6") == false){
+		cout << R"(
+input a mac and an IPv6 Addr
+
+	2 required arguments
+
+	-mac <mac>
+	-ipv6 <ipv6>
+
+)";
+		return 1;
+	}
+
+	// link local   fe80::250:56ff:feaa:8a25
+	// MAC          0050:56aa:8a25
+	//              fe80::250:56ff:feaa:8a25
+
+	string ipv6_start = os.key_value("-ipv6",0);
+	string mac_str  = re::sub(":","",os.key_value("-mac",0));
+
+
+	if (re::scan("::", mac_str) || re::count("::",mac_str) > 5){
+		cout << "It looks like you may have used the -ipv6 in place of the -mac\n";
+	}
+
+	if((re::count(":", ipv6_start) != 7 && re::scan("::", ipv6_start) == false) || re::count(":",ipv6_start) > 7){
+		cout << "You don't have the right amout of IPv6 segments\n";
+	}
+
+	vector<string> split_ipv6 = re::split(":",ipv6_start);
+	int colons = re::char_count(':',ipv6_start); // 
+
+	vector<string> ipv6_vec;
+
+
+	// Normalize IPv6
+	for(string& i : split_ipv6){
+		if(i.size() > 4){ cout << "Invalid IPv6 syntax\n"; return 1; }
+		while(i.size() < 4)
+			i = string("0") + i;
+		ipv6_vec.push_back(i);
+	}
+
+	// Modify MAC
+	string mac_injection("ff:fe");
+
+	mac_str.insert(mac_str.begin()+6,mac_injection.begin(), mac_injection.end());
+
+	string mac_first_byte_str;
+	mac_first_byte_str.insert(mac_first_byte_str.begin(), mac_str.begin(), mac_str.begin()+2);
+
+	std::stringstream ss;
+	int mac_first_byte_int;
+	ss << std::hex << mac_first_byte_str;
+	ss >> mac_first_byte_int;
+	std::bitset<8> mac_first_byte_bits(mac_first_byte_int);
+	mac_first_byte_bits.flip(1);
+	ss.clear();
+	ss << std::hex << mac_first_byte_bits.to_ulong() << endl;
+	ss >> mac_first_byte_str;
+	mac_first_byte_str = (mac_first_byte_str.size() == 1)? (std::string("0")+mac_first_byte_str) : mac_first_byte_str;
+	
+	mac_str.erase(mac_str.begin(),mac_str.begin()+2);
+	mac_str.insert(mac_str.begin(),mac_first_byte_str.begin(), mac_first_byte_str.end());	
+	mac_str.insert(mac_str.begin()+4, ':');
+	mac_str.insert(mac_str.begin()+14, ':');
+
+
+	// Inject MAC into IPv6
+
+	vector<string> mac_segs = re::split(":",mac_str);
+
+	string ipv6_out = ipv6_vec[0]+"::";
+	for(string& i: mac_segs)
+		ipv6_out += (i + string(":"));
+
+	ipv6_out.erase(ipv6_out.end()-1);
+	ipv6_out = re::sub(":0",":",ipv6_out);
+	
+
+	cout << "mac addr   = " << os.key_value("-mac", 0) << endl;
+	cout << "input IPv6 = " << os.key_value("-ipv6", 0) << endl;
+	cout << "link local = " << ipv6_out << endl;
+
+	return 0;
+}
---
+++ Multi/calc_ipv6/comp_ipv6.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+
+x86_64-linux-gnu-g++ -g calc_ipv6.cpp -o calc_ipv6 -std=c++17 -Wfatal-errors -lOS -lord -lre
+
